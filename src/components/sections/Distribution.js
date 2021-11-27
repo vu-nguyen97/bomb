e927eb91e41bbfd547defcb6fb59e1fae5e37ba7
vu.nguyenvan <vunv@falcongames.com>
2021-11-27 16:55:18 +0700

    Add donut chart

--- src/components/sections/Distribution.js
+++ src/components/sections/Distribution.js
@@ -1,6 +1,10 @@
 import React from "react";
-import DonutChat from "../../assets/images/landing-page/pitchdeck/donut-chat.png";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const thList = [
   { title: "Allocation type", priority: 1, name: "allocation" },
@@ -16,7 +20,7 @@ const thList = [
   { title: "Vesting type", priority: 3, name: "vesting type" },
 ];
 
-const dataList = [
+const tableDataList = [
   ["Seed Round", "30,000,000", "3%", "5%", 3, 18, "Monthly"],
   ["Private Sale", "100,000,000", "10%", "5%", 3, 24, "Monthly"],
   ["Public Sale", "10,000,000", "1%", "100%", " ", " ", " "],
@@ -29,13 +33,71 @@ const dataList = [
   ["Total Supply", "1,000,000,000", "100%", " ", " ", " ", " "],
 ];
 
+const chartData = {
+  labels: [
+    "Seed Round",
+    "Private Sale",
+    "Public Sale",
+    "DEX Liquidity",
+    "Advisor",
+    "Community Fund",
+    "Falcon Team",
+    "Play To Earn",
+    "Staking Reward",
+  ],
+  datasets: [
+    {
+      data: [3, 10, 1, 5, 3, 12, 21, 23, 22],
+      backgroundColor: [
+        "rgb(19, 141, 117)",
+        "rgb(52, 195, 143)",
+        "rgb(85, 110, 230)",
+        "rgb(93, 173, 226)",
+        "rgb(30, 132, 73)",
+        "rgb(27, 79, 114)",
+        "rgb(24, 106, 59)",
+        "rgb(21, 67, 96)",
+        "rgb(69, 179, 157)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: "right",
+      labels: {
+        color: "#dbeae8",
+        padding: 15,
+      },
+    },
+    tooltip: {
+      callbacks: {
+        label: function (context) {
+          const { label, parsed } = context || "";
+
+          return `${label}: ${parsed}%`;
+        },
+      },
+    },
+  },
+};
+
 function Distribution() {
   return (
     <section className="Pitchdeck full-viewport d-flex flex-column justify-content-end">
       <div className="container reveal-from-bottom" data-reveal-delay="200">
-        <div className="d-flex flex-column align-items-start justify-content-center">
-          <div className="h5 mb-0 text-font">Distribution planning</div>
-          <img src={DonutChat} className="Pitchdeck-img" alt="donut chat" />
+        <div className="row">
+          <div className="col-2 d-none d-md-block" />
+          <div className="col-md-6 col-xs-12">
+            <div className="h5 mb-0 text-font">Distribution planning</div>
+            <div className="Pitchdeck-chart">
+              <Doughnut data={chartData} options={options} />
+            </div>
+          </div>
         </div>
       </div>
 
@@ -57,7 +119,7 @@ function Distribution() {
             </Tr>
           </Thead>
           <Tbody>
-            {dataList.map((data, dataId) => (
+            {tableDataList.map((data, dataId) => (
               <Tr key={dataId}>
                 {data.map((field, id) => (
                   <Td key={id}>{field}</Td>
