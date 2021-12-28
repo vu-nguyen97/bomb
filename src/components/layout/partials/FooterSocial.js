9e1fef346a06e5ef0f7803aa422aeb5e42b4f2a5
vu.nguyenvan <vunv@falcongames.com>
2021-12-28 13:37:35 +0700

    Add download buttons

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -4,26 +4,39 @@ import fb from "../../../assets/images/landing-page/footer/fb.png";
 import discord from "../../../assets/images/landing-page/footer/discord.png";
 import telegram from "../../../assets/images/landing-page/footer/telegram.png";
 import twitter from "../../../assets/images/landing-page/footer/twitter.png";
+import youtube from "../../../assets/images/landing-page/footer/youtube.png";
 
 const FooterSocial = ({ className }) => {
   const classes = classNames("footer-social", className);
   const listIcon = [
-    { name: "fb", icon: fb },
-    { name: "discord", icon: discord },
-    { name: "twitter", icon: twitter },
-    { name: "telegram", icon: telegram },
+    { name: "fb", icon: fb, url: "https://www.facebook.com/PiguLand/" },
+    {
+      name: "discord",
+      icon: discord,
+      url: "https://discord.com/channels/923503591719837696/923504349144055858",
+    },
+    { name: "twitter", icon: twitter, url: "https://twitter.com/Pigu_Land" },
+    { name: "telegram", icon: telegram, url: "https://t.me/PiguLandChannel" },
+    {
+      name: "youtube",
+      icon: youtube,
+      url: "https://www.youtube.com/channel/UCrQ0_fEIbYR3Wi_-tOYfCzA",
+    },
   ];
 
   return (
     <div className={classes}>
       <div className="d-flex">
         {listIcon.map((item, id) => (
-          <img
+          <a
+            href={item.url}
             key={id}
-            src={item.icon}
-            alt={item.name}
             className="social-margin"
-          />
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.icon} alt={item.name} />
+          </a>
         ))}
       </div>
     </div>
--- src/components/sections/CommonInfo.js
+++ src/components/sections/CommonInfo.js
@@ -1,8 +1,38 @@
 import React from "react";
+
 import logo from "../../assets/images/landing-page/logo.png";
+import apple from "../../assets/images/landing-page/common-info/apple.png";
+import chPlay from "../../assets/images/landing-page/common-info/ch-play.png";
+import windowLogo from "../../assets/images/landing-page/common-info/window.png";
+
 import videoBg from "../../assets/videos/home-video.mp4";
 
-const CommonInfo = (props) => {
+const downloadBtns = [
+  {
+    name: "apple",
+    icon: apple,
+    line1: "Download on the",
+    line2: "App Store",
+    isComingSoon: true,
+  },
+  {
+    name: "ch play",
+    icon: chPlay,
+    line1: "Get it on",
+    line2: "Google Play",
+    isComingSoon: true,
+  },
+  {
+    name: "window",
+    icon: windowLogo,
+    line1: "Download the",
+    line2: "PC version",
+    isComingSoon: true,
+    classnames: "d-none d-lg-block",
+  },
+];
+
+const CommonInfo = () => {
   const totalSnow = 60;
 
   return (
@@ -16,7 +46,31 @@ const CommonInfo = (props) => {
           src={videoBg}
           className="CommonInfo-videoBg w-100 h-100"
         />
+
         <div className="CommonInfo-blackzone" />
+
+        <div className="CommonInfo-downloadBtnsGroup d-flex justify-content-center">
+          {downloadBtns.map((btn, id) => (
+            <div key={id} className={btn.classnames || ""}>
+              <div className="CommonInfo-downloadBtnsGroup-btnWrapper px-3 py-2 mx-3 d-flex align-items-center cursor-pointer">
+                <img src={btn.icon} alt={btn.name} className="logoImg" />
+                <div className="btn-content ms-2">
+                  <div className="line-1 font-size-12 text-truncate">
+                    {btn.line1}
+                  </div>
+                  <div className="line-2 font-size-22 text-truncate">
+                    {btn.line2}
+                  </div>
+                </div>
+                {btn.isComingSoon && (
+                  <div className="comingSoon d-flex justify-content-center align-items-center font-size-13">
+                    Coming soon...
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
 
         <div
           className="CommonInfo-header d-flex justify-content-center reveal-from-top"
